331295589326d57d44838ab3cddf177e14b1e0ab
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-04 20:18:05 -0400

    update

--- front/src/components/Post.js
+++ front/src/components/Post.js
@@ -3,15 +3,15 @@ import React from 'react'
 const Post = () => {
   return (
     <div>
-        <div class="col">
-    <div class="card" style={{ height: "300px", width: " 250px"}}>
-        <h6>Avatar</h6>
+          <div class="card">
+      <img src="https://images.pexels.com/photos/1150988/pexels-photo-1150988.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top" alt="..." />
       <div class="card-body">
-        <img src="https://images.pexels.com/photos/16034334/pexels-photo-16034334.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top" style={{height: "150px"}} alt="..." />
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
 
       </div>
     </div>
-  </div>
     </div>
   )
 }
